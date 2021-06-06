5a0708a8ce1ceef7908fcb4b69ed4742ada27f7c
travis <travis@techtaka.com>
2021-06-06 13:05:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springbootreactivets/SpringBootReactiveTsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactivets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveTsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveTsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootreactivets/SpringBootReactiveTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactivets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveTsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
